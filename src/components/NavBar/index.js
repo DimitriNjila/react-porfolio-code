67e7bfc51c4cc20455a0d5cc36f88e2b09bf1db3
Dymytry77 <77091028+Dymytry77@users.noreply.github.com>
2021-10-14 13:41:17 -0400

    initial commit

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Container, LogoContainer, Nav, NavItem, NavButton } from './NavBarElements';
+
+
+const NavBar = () => {
+    return (
+        <>
+         <Container>
+             <LogoContainer>
+
+             </LogoContainer>
+             <Nav>
+                 <NavItem>
+
+                 </NavItem>
+                 <NavItem>
+                     
+                 </NavItem>
+                 <NavItem>
+                     
+                 </NavItem>
+                 <NavItem>
+                     
+                 </NavItem>
+                 <NavItem>
+                     
+                 </NavItem>
+
+                 <NavButton>
+
+                 </NavButton>
+             </Nav>
+        </Container>   
+        </>
+    )
+}
+
+export default NavBar
